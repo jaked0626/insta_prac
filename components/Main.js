7ca45679e7d86b085647abe6b58d53cc0bb720f0
jaked0626 <jakez0626@gmail.com>
2022-04-05 17:38:53 +0900

    customize camera button

--- components/Main.js
+++ components/Main.js
@@ -1,32 +1,18 @@
 // file that is called after Landing, Login, and Register
+// import react and react elements
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import Entypo from 'react-native-vector-icons/Entypo';
-import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
-import Fontisto from 'react-native-vector-icons/Fontisto';
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import { NavigationContainer } from '@react-navigation/native';
+
+// load page content
+import Tabs from './main/Tabs';
+
 // load redux libraries
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchUser } from '../redux/actions/index';
 
-// load components in main/
-// camera found in App because it is a new screen
-import FeedScreen from './main/Feed.js';
-import GraphScreen from './main/Graph.js';
-import ProfileScreen from './main/Profile.js';
-import ScoreboardScreen from './main/Scoreboard.js';
 
-
-// create bottom navigation tab
-const Tab = createBottomTabNavigator();
-
-const EmptyScreen = () => {
-    return(null); // placeholder for camera
-}
 
 export class Main extends Component {
     componentDidMount(){
@@ -35,63 +21,15 @@ export class Main extends Component {
     }
     render() {
         const { currentUser } = this.props; // redux calls current user info, fetched earlier
-        console.log(currentUser);
+        //console.log(currentUser);
 
         if (currentUser == undefined) {
             return(<View style = {{ flex:1, justifyContent: 'center', alignItems: 'center' }}>
                         <Text textAlign={'center'}>Fetching User Info...</Text>
-                   </View>) // leave blank
+                   </View>) 
         }
 
-        return (
-            <Tab.Navigator
-                initialRouteName='Graph'
-                labeled = { false }
-                screenOptions={{
-                    tabBarShowLabel : false,
-                    tabBarActiveTintColor : '#A3E4D7',
-                    tabBarActiveBackgroundColor: "#808B96",
-                    tabBarInactiveBackgroundColor: '#808B96',
-                    tabBarInactiveTintColor: 'white',
-                }}>
-                <Tab.Screen name="Feed" component={FeedScreen}
-                    options = {{
-                        tabBarIcon: ({ color, size }) => (
-                            <Fontisto name = 'picture' color = { color } size = { 20 } />
-                        )
-                }} />
-                <Tab.Screen name="Graph" component={GraphScreen}
-                    options = {{
-                        tabBarIcon: ({ color, size }) => (
-                            <Entypo name = 'line-graph' color = { color } size = { 23 } />
-                        )
-                }} />
-                <Tab.Screen name="CameraContainer" component={EmptyScreen}
-                    listeners = {({ navigation }) => ({
-                        tabPress: event => {
-                            event.preventDefault();
-                            navigation.navigate("Camera")
-                        } // https://www.youtube.com/watch?v=gPaBicMaib4&ab_channel=PradipDebnath use to enhance camera tab
-                    })}
-                    options = {{
-                        tabBarIcon: ({ color, size }) => (
-                            <SimpleLineIcons name = 'camera' color = { color } size = { 33 } />
-                        )
-                }} />
-                <Tab.Screen name="Scoreboard" component={ScoreboardScreen}
-                    options = {{
-                        tabBarIcon: ({ color, size }) => (
-                            <FontAwesome5 name = 'medal' color = { color } size = { 22 } />
-                        )
-                }} />
-                <Tab.Screen name="Profile" component={ProfileScreen}
-                    options = {{
-                        tabBarIcon: ({ color, size }) => (
-                            <Ionicons name = 'person-circle' color = { color } size = { 27 } />
-                        )
-                }} />
-            </Tab.Navigator>
-                    )
+        return (<Tabs />)
     }
 }
 
